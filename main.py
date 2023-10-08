d4bbaef6150c4f75b91738a53cd77fada899bd5a
alex-pran <apranitchii@gmail.com>
2023-10-08 11:23:12 -0400

    variable concatination print text

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ message = "I am variable value and can be changed"
 
 print(message)
 print('Second' + message)
+
+num1 = 25
+num2 = 30.45 #float
+status = True
+
+print('data type ', type(message))  #you can concatenate (join multiple text)
+print('data type ', type(num1))
+print('data type ',type(num2))
+print('data type ',type(status))
